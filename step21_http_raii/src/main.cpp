a883462a1878d575813954e724d465cc830359d2
Toru Hara <toruh@deluxe.ocn.ne.jp>
2026-07-12 15:14:00 +0900

    nlohmann, cmake installed at mac, http_get separated

--- step21_http_raii/src/main.cpp
+++ step21_http_raii/src/main.cpp
@@ -64,37 +64,17 @@ int main(){
         std::cerr << "curl_global_init failed\n";
         return 1;
     }
-    CURL* handle = curl_easy_init();
+    long status_code = 0;
+    std::string body;
 
-    if (handle == nullptr) {
-        std::cerr << "curl_easy_init failed\n";
+    CURLcode result = http_get("https://jsonplaceholder.typicode/posts/1", status_code, body);
+
+    if (result != CURLE_OK) {
+        std::cerr << "curl failed: " << curl_easy_strerror(result) << '\n';
         curl_global_cleanup();
         return 1;
     }
 
-    CURLcode result = curl_easy_setopt(handle, CURLOPT_URL, "https://jsonplaceholder.typicode.com/posts/1");
-
-    std::string body;
-
-    if (result == CURLE_OK) {
-        result = curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, save_to_data);
-    }
-    if (result == CURLE_OK) {
-        result = curl_easy_setopt(handle, CURLOPT_WRITEDATA, &body);
-    }
-
-    if (result == CURLE_OK) {
-        result = curl_easy_setopt(handle, CURLOPT_WRITEDATA, &body);
-    }
-    
-    long status_code = 0;
-    if (result == CURLE_OK) {
-        result = curl_easy_perform(handle);
-    }
-
-    if (result == CURLE_OK) {
-        result = curl_easy_getinfo(handle, CURLINFO_HTTP_CODE, &status_code);
-    }
 
     if (result == CURLE_OK) {
         nlohmann::json result_json = nlohmann::json::parse(body);
@@ -104,14 +84,9 @@ int main(){
         std::cout << "title: " << std::string(result_json["title"]) << '\n';
 
     }
-    else {
-        std::cerr << "curl failed: " << curl_easy_strerror(result) << '\n';
-    }
-
-    curl_easy_cleanup(handle);
     curl_global_cleanup();
 
-    return result == CURLE_OK ? 0 : 1;
+    return 1;
 
 
 }
